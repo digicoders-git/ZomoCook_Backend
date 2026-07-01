d318887fb1ae5e575ac088412ac75d1d56af87a2
Er.Mayank Pandey <mp04042007@gmail.com>
2026-07-02 02:55:03 +0530

    firebase key split/join approach

--- config/firebase.js
+++ config/firebase.js
@@ -1,13 +1,18 @@
 const admin = require('firebase-admin');
 
 if (!admin.apps.length) {
-    const privateKey = (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n');
+    let privateKey = process.env.FIREBASE_PRIVATE_KEY || '';
+    privateKey = privateKey.replace(/^\"|\"$/g, '').replace(/^'|'$/g, '');
+    privateKey = privateKey.split('\\n').join('\n');
+
+    console.log('[Firebase] Key starts with:', privateKey.substring(0, 27));
+    console.log('[Firebase] Has real newlines:', privateKey.includes('\n'));
+
     admin.initializeApp({
         credential: admin.credential.cert({
             projectId: process.env.FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey
-            
+            privateKey: privateKey
         })
     });
     console.log('[Firebase] Initialized. Project:', process.env.FIREBASE_PROJECT_ID);
