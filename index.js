3ef53d4a8593b98df5279af5ad9d36b27df94646
ErMayankPandey2007 <mp042007@gmail.com>
2026-07-15 13:18:32 +0530

    fix: import path module in backend index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 
 // Load env vars FIRST before any other imports that use process.env
 dotenv.config();
